a69b3cfd8d636f15ca5c008e1e970c51c822cd68
adouch9 <mr.ouldmoussa@gmail.com>
2021-04-08 15:00:08 +0200

    monty hall game

---
+++ montyHall.js
@@ -0,0 +1,4 @@
+const fs             = require('fs')                                  
+const { randomInt }     = require('crypto')
+const readlineSync      = require('readline-sync')
+const chalk             = require('chalk');
